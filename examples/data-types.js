f7db8aaf18682825b7371ca46d2255d67cdef4a0
Nehal <npatel3030@gmail.com>
2021-09-05 10:55:41 -0400

    fin data-types?

---
+++ examples/data-types.js
@@ -0,0 +1,36 @@
+// Strings
+console.log("Strings!");
+console.log(String(123321));
+
+// Numbers
+console.log(123);
+console.log(3.14159265);
+console.log(Number(123));
+console.log(Number('123'));
+
+// BigInt
+console.log(10000000000000000000000000000000n);
+console.log(BigInt("10000000000000000000000000000000"));
+
+// Booleans
+console.log(true);
+console.log(false);
+console.log(Boolean(true));
+
+// Undefined
+console.log(undefined);
+let x;
+console.log(x);
+function emptyArg(a) {
+    console.log(a);
+}
+emptyArg();
+
+// Symbols
+console.log(Symbol());
+console.log(Symbol("with string"));
+
+// Null
+console.log(null);
+let y = null;
+console.log(x);
